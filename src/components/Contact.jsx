609214e060655f4e45286a0ba34c16303473bd40
Agustin Morro <agustin.morro@gmail.com>
2024-04-10 15:34:15 -0300

    test

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -29,7 +29,7 @@ const Contact = () => {
     <>
       <section
         id="contact"
-        className="realative bg-gray-100 w-full h-screen flex flex-col justify-center items-center lg:items-start lg:pl-20  "
+        className="realative z-50 bg-gray-100 w-full h-screen flex flex-col justify-center items-center lg:items-start lg:pl-20  "
       >
      {/*    <article className="relative flex flex-col">
           <h2 className="w-fit  text-6xl font-semibold font-title2 text-red-700 sm:text-8xl lg:text-9xl">
